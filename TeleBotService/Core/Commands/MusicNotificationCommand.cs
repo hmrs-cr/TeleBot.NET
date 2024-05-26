f9c70adf1d5eb1dd61197b5a6edd7a43cef503b5
hmrs-cr <hmrs-cr@users.noreply.github.com>
2024-05-25 22:12:39 -0600

    Added player status change notifications

---
+++ TeleBotService/Core/Commands/MusicNotificationCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using Microsoft.Extensions.Options;
+using TeleBotService.Config;
+using TeleBotService.Extensions;
+using Telegram.Bot.Types;
+
+namespace TeleBotService.Core.Commands;
+
+public class MusicNotificationCommand : MusicPlayerCommandBase
+{
+    public MusicNotificationCommand(IOptions<MusicPlayersConfig> config, IOptions<TapoConfig> tapoConfig) : base(config, tapoConfig)
+    {
+    }
+
+    public override bool CanExecuteCommand(Message message) => ContainsText(message, "/musicnotify") && (ContainsText(message, "begin") || ContainsText(message, "end"));
+    protected override Task<int> ExecuteMusicPlayerCommand(Message message, PlayersConfig playersConfig, MusicPlayersPresetConfig? musicPlayersPresetConfig, CancellationToken cancellationToken = default)
+    {
+        if (ContainsText(message, "begin"))
+        {
+            _ = message.GetContext().NotifyPlayerStatusChanges(playersConfig.Client, async (client, prevStatus, currentStatus) =>
+            {
+                var same = prevStatus?.Title == currentStatus?.Title;
+                await ReplyPlayerStatus(message, playersConfig);
+                Console.WriteLine($"Player status New: {currentStatus?.Title}, Prev: {prevStatus?.Title}");
+            });
+        }
+        else if (ContainsText(message, "end"))
+        {
+            _ = message.GetContext().NotifyPlayerStatusChanges(playersConfig.Client, null);
+        }
+
+        return DoNotReplyPlayerStatusTask;
+    }
+}
--- TeleBotService/Core/Model/TelegramChatContext.cs
+++ TeleBotService/Core/Model/TelegramChatContext.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Concurrent;
+using Linkplay.HttpApi;
+using Linkplay.HttpApi.Model;
 using Telegram.Bot.Types;
 
 namespace TeleBotService.Model;
@@ -9,6 +11,8 @@ public class TelegramChatContext
 
     private readonly ChatContextKey key;
 
+    private CancellationTokenSource? playerStatusNotificationCts;
+
     private TelegramChatContext(ChatContextKey key)
     {
         this.key = key;
@@ -18,13 +22,69 @@ public class TelegramChatContext
 
     public string? Username => this.key.Chat?.Username;
 
-    public string LanguageCode { get; set; } = "es";
-
+    public string LanguageCode { get; set; } = "en";
+    public bool IsNotifingPlayerStatusChanges => this.playerStatusNotificationCts != null && !this.playerStatusNotificationCts.IsCancellationRequested;
     public override int GetHashCode() => this.key.GetHashCode();
 
     public override bool Equals(object? obj) => this.key.Equals(obj);
 
     public static TelegramChatContext GetContext(Chat chat) => currentChats.GetOrAdd(new (chat), (key) => new TelegramChatContext(key));
+
+    public async Task NotifyPlayerStatusChanges(LinkplayHttpApiClient client, Action<LinkplayHttpApiClient, PlayerStatus?, PlayerStatus?>? notify)
+    {
+        if (notify == null)
+        {
+            if (this.IsNotifingPlayerStatusChanges)
+            {
+                this.playerStatusNotificationCts.Cancel();
+                Console.WriteLine("Unregistered player status change notification");
+            }
+        }
+        else if (this.playerStatusNotificationCts == null || this.playerStatusNotificationCts.IsCancellationRequested)
+        {
+            Console.WriteLine("Starting player status change notification");
+            this.playerStatusNotificationCts = new();
+            PlayerStatus? prevPlayerStatus = null;
+            while (!this.playerStatusNotificationCts.IsCancellationRequested)
+            {
+                try
+                {
+                    var status = await client.GetPlayerStatus();
+                    if (status == null)
+                    {
+                        if (prevPlayerStatus != null)
+                        {
+                            prevPlayerStatus = null;
+                            notify(client, prevPlayerStatus, null);
+                        }
+                        await Task.Delay(10000, this.playerStatusNotificationCts.Token);
+                    }
+                    else
+                    {
+                        var delay = ((status.Totlen - status.Curpos) / 2) + 1500;
+
+                        if (prevPlayerStatus?.Title != status.Title ||
+                            prevPlayerStatus?.Artist != status.Artist ||
+                              prevPlayerStatus?.Album != status.Album ||
+                                prevPlayerStatus?.Status != status.Status)
+                        {
+                            notify(client, prevPlayerStatus, status);
+                            prevPlayerStatus = status;
+                        }
+
+
+                        Console.WriteLine($"Status notification delay: {delay / 1000}s");
+                        await Task.Delay(delay, this.playerStatusNotificationCts.Token);
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            }
+
+            Console.WriteLine("Ending player status change notification");
+        }
+    }
 
     private class ChatContextKey
     {
