957261a43b55f0a837b1ce4ecf58023fa7935f1f
hmrs-cr <hmrs-cr@users.noreply.github.com>
2024-06-08 18:23:36 -0600

    Code organization

---
+++ TeleBotService/Data/Redis/RedisDataExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using StackExchange.Redis;
+
+namespace TeleBotService.Data.Redis;
+
+public static class RedisDataExtensions
+{
+    // .Select(s => new HashEntry(s.Key, s.Value)).ToArray();
+    public static HashEntry[] ToRedisHashSet<T>(this T obj, Func<PropertyInfo, bool>? propertyFilter = null) =>
+        typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                 .Where(p => p.CanRead)
+                 .Where(p => propertyFilter == null || propertyFilter.Invoke(p))
+                 .Select(p => new HashEntry(p.Name, p.GetValue(obj)?.ToString()))
+                 .ToArray();
+}
---
+++ TeleBotService/Data/Redis/RegistrationExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace TeleBotService.Data.Redis;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddRedisRepositories(this IServiceCollection services, IConfigurationManager configuration) =>
+        services.Configure<RedisConfig>(configuration.GetSection(RedisConfig.RedisConfigName))
+                .AddSingleton<IUsersRepository, UsersRedisRepository>()
+                .AddSingleton<IInternetRadioRepository, InternetRadioRedisRepository>()
+                .AddSingleton<LazyRedis>();
+
+}
