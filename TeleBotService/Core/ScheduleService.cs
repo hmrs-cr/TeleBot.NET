d5d2f1dac97e6886c1dab79c58705775c7a4b80b
hmrs-cr <hmrs-cr@users.noreply.github.com>
2024-07-21 21:45:06 -0600

    Added ScheduleService logger

--- TeleBotService/Core/ScheduleService.cs
+++ TeleBotService/Core/ScheduleService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using Quartz;
 using Quartz.Impl;
+using Quartz.Logging;
 using Quartz.Spi;
 using TeleBotService.Config;
 
@@ -12,10 +13,11 @@ public class SchedulerService : IHostedService
     private readonly SchedulesConfig config;
     private IScheduler? scheduler;
 
-    public SchedulerService(IServiceProvider serviceProvider, IOptions<SchedulesConfig> config)
+    public SchedulerService(IServiceProvider serviceProvider, IOptions<SchedulesConfig> config, ILogger<SchedulerService> logger)
     {
         this.serviceProvider = serviceProvider;
         this.config = config.Value;
+        LogProvider.SetCurrentLogProvider(new MyLogProvider(logger));
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
@@ -46,8 +48,10 @@ public class SchedulerService : IHostedService
     {
         if (this.scheduler == null)
         {
-            var factory = new StdSchedulerFactory();
-            this.scheduler = await factory.GetScheduler(cancellationToken);
+            this.scheduler = await SchedulerBuilder.Create()
+                                                   .UseDefaultThreadPool(o => o.MaxConcurrency = 2)
+                                                   .BuildScheduler();
+
             scheduler.JobFactory = new JobFactory(this.serviceProvider);
         }
 
@@ -91,7 +95,48 @@ public class SchedulerService : IHostedService
             this.logger.LogDebug("Automatically executed command '{commandText}' by user '{user}' with result '{commandResult}'", config.CommandText, config.User, result);
         }
     }
+
+    private class MyLogProvider : ILogProvider
+    {
+        private static Dictionary<Quartz.Logging.LogLevel, Microsoft.Extensions.Logging.LogLevel> LogLevelMap = new ()
+        {
+            { Quartz.Logging.LogLevel.Debug, Microsoft.Extensions.Logging.LogLevel.Debug },
+            { Quartz.Logging.LogLevel.Error, Microsoft.Extensions.Logging.LogLevel.Error },
+            { Quartz.Logging.LogLevel.Fatal, Microsoft.Extensions.Logging.LogLevel.Critical },
+            { Quartz.Logging.LogLevel.Info, Microsoft.Extensions.Logging.LogLevel.Information },
+            { Quartz.Logging.LogLevel.Trace, Microsoft.Extensions.Logging.LogLevel.Trace },
+            { Quartz.Logging.LogLevel.Warn, Microsoft.Extensions.Logging.LogLevel.Warning },
+        };
+
+        private readonly ILogger logger;
+
+        public MyLogProvider(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public Logger GetLogger(string name) => (level, func, exception, parameters) =>
+        {
+            if (func != null)
+            {
+                var message = func();
+                this.logger.Log(LogLevelMap[level], exception, message, parameters);
+            }
+            return true;
+        };
+
+        public IDisposable OpenNestedContext(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable OpenMappedContext(string key, object value, bool destructure = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
+
 
 public static class SchedulerRegistrationExtensions
 {
