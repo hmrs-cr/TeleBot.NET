75f56db69aed83096eeebb046be3c7d012598031
hmrs-cr <hmrs-cr@users.noreply.github.com>
2024-07-28 09:28:12 -0600

    Added 'schedule' option to 'GetJobs' command

--- TeleBotService/Core/Commands/Admin/JobInfoCommand.cs
+++ TeleBotService/Core/Commands/Admin/JobInfoCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Text.Json;
+using Humanizer;
 using TeleBotService.Core.Model;
 
 namespace TeleBotService.Core.Commands.Admin;
@@ -26,14 +27,35 @@ public class JobInfoCommand : TelegramCommand
     protected override async Task Execute(MessageContext messageContext, CancellationToken cancellationToken = default)
     {
         var jobs = this.jobInfoProvider.GetJobs();
-        using var stream = new MemoryStream();
-        await JsonSerializer.SerializeAsync(utf8Json: stream, jobs, this.jsonSerializerOptions, cancellationToken: cancellationToken);
-        stream.Position = 0;
-        using var streamReader = new StreamReader(stream, Encoding.UTF8);
-        string result = streamReader.ReadToEnd();
+        var sb = new StringBuilder().Append("<pre>");
 
-        var sb = new StringBuilder();
-        sb.Append("<pre>").Append(result).Append("</pre>");
+        if (messageContext.Message.Text?.Contains("schedule", StringComparison.InvariantCultureIgnoreCase) == true)
+        {
+            await foreach (var job in jobs)
+            {
+                DateTimeOffset? previousFireTimeUtc = null;
+                DateTimeOffset? nextFireTimeUtc = null;
+                if (job.Triggers.FirstOrDefault() is { } defaultTrigger)
+                {
+                    previousFireTimeUtc = defaultTrigger.PreviousFireTimeUtc;
+                    nextFireTimeUtc = defaultTrigger.NextFireTimeUtc;
+                }
+
+                sb.Append(job.JobKey).Append("  PrevExec: ").Append(previousFireTimeUtc.Humanize()).Append(", NexExec: ").Append(nextFireTimeUtc.Humanize())
+                  .AppendLine();
+            }
+        }
+        else
+        {
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(utf8Json: stream, jobs, this.jsonSerializerOptions, cancellationToken: cancellationToken);
+            stream.Position = 0;
+            using var streamReader = new StreamReader(stream, Encoding.UTF8);
+            var result = streamReader.ReadToEnd();
+            sb.Append(result);
+        }
+
+        sb.Append("</pre>");
         await this.ReplyFormated(messageContext, sb.ToString(), cancellationToken);
     }
 }
